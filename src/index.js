b07d1eea07d20bb3566a9b4abaca1118814214e0
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2021-11-16 17:31:49 +0900

    Add : React 인스타그램

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from "./Router"
+
+import "./pages/Main/Main.scss";
+import "./pages/Login/Login.scss";
+import "./styles/common.scss";
+
+ReactDOM.render(<Router />, document.getElementById("root"));
+
+
